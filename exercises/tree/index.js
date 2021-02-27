18524b82b57ff01b774f589cc40f38142df5c1b2
Dar Dualba <ddualba@gmail.com>
2021-02-26 22:03:27 -0800

    finish tree and tree width excercise

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,47 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+  add(data) {
+    let node = new Node(data);
+    this.children.push(node);
+  }
 
-class Tree {}
+  remove(data) {
+    this.children = this.children.filter(child => {
+      child !== data;
+    });
+  }
+}
+
+class Tree {
+  constructor(data) {
+    this.root = null;
+  }
+  traverseBF(fn) {
+    const arr = [this.root];
+
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+  traverseDF(fn) {
+    const arr = [this.root];
+
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
+}
 
 module.exports = { Tree, Node };
